08ec6efba9be1aa92fbb4659fa86148f20913952
mustafos <mustafos1999@icloud.com>
2024-04-11 09:05:49 +0200

    init project

---
+++ MarsSnap/MarsSnap/MarsSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsSnapApp.swift
+//  MarsSnap
+//
+//  Created by Mustafa Bekirov on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MarsSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
